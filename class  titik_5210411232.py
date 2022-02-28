59a23a41fa69fd5f352fc103504564634e1091e3
Taufiksk2 <99322664+Taufiksk2@users.noreply.github.com>
2022-02-28 20:53:09 +0700

    Add files via upload

---
+++ class  titik_5210411232.py
@@ -0,0 +1,11 @@
+#5210411232
+
+class Titik:
+  def__init__(, x, y):
+    self.x = x
+    self.y = y
+
+
+titik_a = Titik(0,0)
+titik_b = Titik(3,4)
+print('pnjang garis ab adalah {}'.format(garis_ab.panjang()))
